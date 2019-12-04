bc4db91b896face0b98c6009b89ead29635b3281
Dima <dima.sapsay@gmail.com>
2019-12-04 11:54:50 +0200

    first commit

--- zoo.py
+++ zoo.py
@@ -1,14 +1,65 @@
+"""
+There are three kinds of animals in a zoo.
+All of them have names. Also, dogs say "woof", cats say "meow",
+birds say "tweet". Implement classes for all kinds of animals
+according to tests.
+"""
+
+
 class Zoo:
-    pass
+    """Class Zoo"""
+    def __init__(self):
+        self.zoo_lst = []
+
+    def add(self, animal):
+        """Add animal to zoo"""
+        self.zoo_lst.append(animal)
+
+    def get_animals(self):
+        """Get all animals"""
+        return self.zoo_lst
 
 
 class Dog:
-    pass
+    """Class Dog"""
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        """Get name animal"""
+        return self.name
+
+    @staticmethod
+    def say():
+        """Animal say"""
+        return "woof"
 
 
 class Cat:
-    pass
+    """Class Cat"""
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        """Get name animal"""
+        return self.name
+
+    @staticmethod
+    def say():
+        """Animal say"""
+        return "meow"
 
 
 class Bird:
-    pass
+    """Class Bird"""
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        """Get name animal"""
+        return self.name
+
+    @staticmethod
+    def say():
+        """Animal say"""
+        return "tweet"
