849c5da15cc273d4146678ee83a633ddfdcfe5dc
d1makov <makovskyj.danylo@gmail.com>
2019-12-04 16:54:11 +0200

    fin

--- zoo.py
+++ zoo.py
@@ -1,14 +1,116 @@
+"""
+docstring
+"""
+
+
 class Zoo:
-    pass
+    """
+    zoo class
+    """
+    def __init__(self):
+        """
+        def animals list
+        """
+        self.animals = []
+
+    def get_animals(self):
+        """
+
+        :return:
+        """
+        return self.animals
+
+    def add_animals(self, animal):
+        """
+
+        :param animal:
+        :return:
+        """
+        self.animals.append(animal)
 
 
-class Dog:
-    pass
+class Animal:
+    """
+    animal class
+    """
+    def __init__(self, name):
+        """
+
+        :param name:
+        """
+        self.name = name
+        self.voice = ''
+
+    def get_name(self):
+        """
+
+        :return:
+        """
+        return self.name
+
+    def get_voice(self):
+        """
+
+        :return:
+        """
+        return self.voice
 
 
-class Cat:
-    pass
+class Dog(Animal):
+    """
+    dog class
+    """
+    def __init__(self, name):
+        """
+
+        :param name:
+        """
+        super().__init__(name)
+        self.voice = 'woof'
+
+    def say(self):
+        """
+
+        :return:
+        """
+        return self.voice
 
 
-class Bird:
-    pass
+class Cat(Animal):
+    """
+    cat class
+    """
+    def __init__(self, name):
+        """
+
+        :param name:
+        """
+        super().__init__(name)
+        self.voice = 'meow'
+
+    def say(self):
+        """
+
+        :return:
+        """
+        return self.voice
+
+
+class Bird(Animal):
+    """
+    bird class
+    """
+    def __init__(self, name):
+        """
+
+        :param name:
+        """
+        super().__init__(name)
+        self.voice = 'tweet'
+
+    def say(self):
+        """
+
+        :return:
+        """
+        return self.voice
