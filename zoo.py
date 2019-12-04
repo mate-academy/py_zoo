c478e43f41498215ba1644fda2a8d0f4cc6810dc
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-04 11:16:04 +0200

    1st commit

--- zoo.py
+++ zoo.py
@@ -1,14 +1,60 @@
+"""modules"""
+
+
 class Zoo:
-    pass
+    """A class used to represent zoo"""
+    def __init__(self):
+        self.animals_list = []
+
+    def add(self, animal):
+        """add animal to zoo"""
+        self.animals_list.append(animal)
+
+    def get_animals(self):
+        """return list of animals present in zoo"""
+        return self.animals_list
 
 
 class Dog:
-    pass
+    """A class used to represent dog"""
+    def __init__(self, name):
+        self.name = name
+        self.sound = "woof"
+
+    def get_name(self):
+        """return name of animal"""
+        return self.name
+
+    def say(self):
+        """return sound of animal"""
+        return self.sound
 
 
 class Cat:
-    pass
+    """A class used to represent cat"""
+    def __init__(self, name):
+        self.name = name
+        self.sound = "meow"
+
+    def get_name(self):
+        """return name of animal"""
+        return self.name
+
+    def say(self):
+        """return sound of animal"""
+        return self.sound
 
 
 class Bird:
-    pass
+    """A class used to represent bird"""
+    def __init__(self, name):
+        self.name = name
+        self.sound = "tweet"
+
+    def get_name(self):
+        """return name of animal"""
+        return self.name
+
+    def say(self):
+        """return sound of animal"""
+        return self.sound
