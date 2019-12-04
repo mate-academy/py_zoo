6b00facae3cbcd2d2d755789205a6dfd660ce01d
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-04 15:57:08 +0200

    2nd commit

--- zoo.py
+++ zoo.py
@@ -15,46 +15,37 @@ class Zoo:
         return self.animals_list
 
 
-class Dog:
+class Animal:
+    """class used to represent animal"""
+    def __init__(self, name):
+        self.name = name
+        self.sound = ""
+
+    def get_name(self):
+        """return name of animal"""
+        return self.name
+
+    def say(self):
+        """return sound of animal"""
+        return self.sound
+
+
+class Dog(Animal):
     """A class used to represent dog"""
     def __init__(self, name):
-        self.name = name
+        super().__init__(name)
         self.sound = "woof"
 
-    def get_name(self):
-        """return name of animal"""
-        return self.name
 
-    def say(self):
-        """return sound of animal"""
-        return self.sound
-
-
-class Cat:
+class Cat(Animal):
     """A class used to represent cat"""
     def __init__(self, name):
-        self.name = name
+        super().__init__(name)
         self.sound = "meow"
 
-    def get_name(self):
-        """return name of animal"""
-        return self.name
 
-    def say(self):
-        """return sound of animal"""
-        return self.sound
-
-
-class Bird:
+class Bird(Animal):
     """A class used to represent bird"""
     def __init__(self, name):
-        self.name = name
+        super().__init__(name)
         self.sound = "tweet"
-
-    def get_name(self):
-        """return name of animal"""
-        return self.name
-
-    def say(self):
-        """return sound of animal"""
-        return self.sound
