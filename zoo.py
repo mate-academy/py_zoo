54a77608ec79927243f542a5b982cd4a9494ba13
alexander <earthhuman41@gmail.com>
2019-12-12 00:40:18 +0200

    zoo

--- zoo.py
+++ zoo.py
@@ -1,14 +1,86 @@
+"""
+Zoo, Animal, Dog, Cat, Bird
+"""
+
+
 class Zoo:
-    pass
+    """
+    zoo
+    """
+    def __init__(self):
+        """
+        set animals
+        """
+        self._animals = []
+
+    def add(self, animal):
+        """
+        add
+        """
+        self._animals.append(animal)
+
+    def get_animals(self):
+        """
+        get_animals
+        """
+        return self._animals
 
 
-class Dog:
-    pass
+class Animal:
+    """
+    animal
+    """
+    def __init__(self, name):
+        """
+        set voice
+        """
+        self._name = name
+        self._voice = None
+
+    def get_name(self):
+        """
+        get_name
+        """
+        return self._name
+
+    def say(self):
+        """
+        say
+        """
+        return self._voice
 
 
-class Cat:
-    pass
+class Dog(Animal):
+    """
+    dog
+    """
+    def __init__(self, name):
+        """
+        set voice
+        """
+        super().__init__(name)
+        self._voice = 'woof'
 
 
-class Bird:
-    pass
+class Cat(Animal):
+    """
+    cat
+    """
+    def __init__(self, name):
+        """
+        set voice
+        """
+        super().__init__(name)
+        self._voice = 'meow'
+
+
+class Bird(Animal):
+    """
+    bird
+    """
+    def __init__(self, name):
+        """
+        set voice
+        """
+        super().__init__(name)
+        self._voice = 'tweet'
